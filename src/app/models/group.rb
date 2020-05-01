7e80db173b9e418d2f4fecbc2597d00781fc1ce8
Matthew Zidar <matthewzidar@gmail.com>
2020-04-30 21:33:14 -0400

    Add files via upload

--- src/app/models/group.rb
+++ src/app/models/group.rb
@@ -1,2 +1,26 @@
+# Project name: MusicalStudentsGroup
+# Description: Lets school administrators use a platform to contact donators for instruments,
+# Setup a systematic way for students to receive lessons, and for students to form their own
+# student music groups.
+# Filename: group.rb
+# Description: groups model, function for searching
+# NOTE: This was generated from the scaffold framework and the function was implemented by me
+# Last modified on: 4-30-2020
+
 class Group < ApplicationRecord
+    #------------------------------------------------------------------
+    #
+    # Function: search_groups()
+    # Parameters: name: string
+    # Pre-Condition: name entered must be valid
+    # Post-Condition: List all music groups being offered from someone
+    # with a specific name, otherwise no results are returned
+    # -----------------------------------------------------------------
+    def self.search_groups(search)
+        if search
+          where(["name LIKE ?","%#{search}%"])
+        else
+          all 
+        end
+    end
 end
--- src/app/models/lesson.rb
+++ src/app/models/lesson.rb
@@ -1,2 +1,26 @@
+# Project name: MusicalStudentsGroup
+# Description: Lets school administrators use a platform to contact donators for instruments,
+# Setup a systematic way for students to receive lessons, and for students to form their own
+# student music groups.
+# Filename: lesson.rb
+# Description: lessons model, function for searching
+# NOTE: This was generated from the scaffold framework and the function was implemented by me
+# Last modified on: 4-30-2020
+
 class Lesson < ApplicationRecord
+    #------------------------------------------------------------------
+    #
+    # Function: search_lessons()
+    # Parameters: instrument: string
+    # Pre-Condition: instrument entered must be valid
+    # Post-Condition: List all music lessons with associated instrument
+    # otherwise no results are returned
+    # -----------------------------------------------------------------
+    def self.search_lessons(search)
+        if search
+          where(["instrument LIKE ?","%#{search}%"])
+        else
+          all 
+        end
+    end
 end
